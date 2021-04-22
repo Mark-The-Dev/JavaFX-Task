c507ad658d7ee37baa550d2d61e9db4486b468f2
Mark <SwiftTactics@gmail.com>
2021-04-22 19:57:22 -0400

    added a progress bar and message to the button action

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -6,7 +6,9 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ProgressBar;
 
 public class Controller {
 
@@ -15,20 +17,37 @@ public class Controller {
     @FXML
     private ListView listView;
 
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Label progressLabel;
+
 
     public void initialize(){
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);
+                //Thread.sleep(1000);
 
-                ObservableList<String> employees = FXCollections.observableArrayList(
-                        "Mister Peanutbutter",
+                String[] names = {"Mister Peanutbutter",
                         "Bojack Horseman",
                         "Diane N.",
                         "Todd Chavez",
                         "Jurj Clooners",
-                        "Someone Else");
+                        "Someone Else"};
+
+                ObservableList<String> employees = FXCollections.observableArrayList();
+
+                for(int i=0; i<6; i++){
+                    employees.add(names[i]);
+                    updateMessage("Added " + names[i] + " to the list");
+                    updateProgress(i + 1, 6);
+                    Thread.sleep(200);
+                }
+
+
+
                 return employees;
             }
         };
@@ -45,6 +64,8 @@ public class Controller {
 //        };
 
         listView.itemsProperty().bind(task.valueProperty());
+        progressBar.progressProperty().bind(task.progressProperty());
+        progressLabel.textProperty().bind(task.messageProperty());
     }
 
     @FXML
