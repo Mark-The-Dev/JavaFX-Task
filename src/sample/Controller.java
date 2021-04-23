2956daa87cdb0d28dca5ca57564980241801d0f6
Mark <SwiftTactics@gmail.com>
2021-04-22 20:07:20 -0400

    added Task to a service

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -4,7 +4,9 @@ import javafx.application.Platform;
 import javafx.beans.Observable;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.concurrent.Service;
 import javafx.concurrent.Task;
+import javafx.concurrent.Worker;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
@@ -12,7 +14,6 @@ import javafx.scene.control.ProgressBar;
 
 public class Controller {
 
-    private Task<ObservableList<String>> task;
 
     @FXML
     private ListView listView;
@@ -23,34 +24,39 @@ public class Controller {
     @FXML
     private Label progressLabel;
 
+    private Service<ObservableList<String>> service;
+
 
     public void initialize(){
-        task = new Task<ObservableList<String>>() {
-            @Override
-            protected ObservableList<String> call() throws Exception {
-                //Thread.sleep(1000);
-
-                String[] names = {"Mister Peanutbutter",
-                        "Bojack Horseman",
-                        "Diane N.",
-                        "Todd Chavez",
-                        "Jurj Clooners",
-                        "Someone Else"};
-
-                ObservableList<String> employees = FXCollections.observableArrayList();
-
-                for(int i=0; i<6; i++){
-                    employees.add(names[i]);
-                    updateMessage("Added " + names[i] + " to the list");
-                    updateProgress(i + 1, 6);
-                    Thread.sleep(200);
-                }
 
 
 
-                return employees;
-            }
-        };
+        //        task = new Task<ObservableList<String>>() {
+//            @Override
+//            protected ObservableList<String> call() throws Exception {
+//                //Thread.sleep(1000);
+//
+//                String[] names = {"Mister Peanutbutter",
+//                        "Bojack Horseman",
+//                        "Diane N.",
+//                        "Todd Chavez",
+//                        "Jurj Clooners",
+//                        "Someone Else"};
+//
+//                ObservableList<String> employees = FXCollections.observableArrayList();
+//
+//                for(int i=0; i<6; i++){
+//                    employees.add(names[i]);
+//                    updateMessage("Added " + names[i] + " to the list");
+//                    updateProgress(i + 1, 6);
+//                    Thread.sleep(200);
+//                }
+//
+//
+//
+//                return employees;
+//            }
+//        };
 
         // one way to do it without bind.
 //                Platform.runLater(new Runnable() {
@@ -63,13 +69,21 @@ public class Controller {
 //            }
 //        };
 
-        listView.itemsProperty().bind(task.valueProperty());
-        progressBar.progressProperty().bind(task.progressProperty());
-        progressLabel.textProperty().bind(task.messageProperty());
+        service = new EmployeeService();
+
+        listView.itemsProperty().bind(service.valueProperty());
+        progressBar.progressProperty().bind(service.progressProperty());
+        progressLabel.textProperty().bind(service.messageProperty());
     }
 
     @FXML
     public void buttonPressed(){
-        new Thread(task).start();
+        if (service.getState() == Worker.State.SUCCEEDED){
+            service.reset();
+            service.start();
+        } else if (service.getState() == Worker.State.READY){
+            service.start();
+        }
+        
     }
 }
